449dceffe88e19a0daced2b6db257f9fcf8a6afe
Rodrigo Alexandre <rodrigo.alexandre@vertem.digital>
2026-02-03 14:24:21 -0300

    chore: remove unused model index and coin service

--- apps/MajoryaRPG/src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './common';
-export * from './character';
-export * from './adventure';
-export * from './npc';
-export * from './creature';
-export * from './map';
-export * from './session';
